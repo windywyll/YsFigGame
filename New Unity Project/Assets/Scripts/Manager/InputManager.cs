973f919481991de49d83cc02db0e2a5a0b16230d
unknown <ce.brun@J001-0128-04.etudiant.rubika-edu.com.local>
2015-11-25 11:07:54 +0100

    Corrections mineurs input manager & Player Manager

--- New Unity Project/Assets/Scripts/Manager/InputManager.cs
+++ New Unity Project/Assets/Scripts/Manager/InputManager.cs
@@ -24,74 +24,26 @@ public class InputManager : MonoBehaviour
 
         touches = Input.touches;
 
-        if(!allPlayersTouching())
-        {
-            assignNewId();
-        }
-
-#if UNITY_EDITOR
-        Debug.Log("testo");
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 mousPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            playerManager.movePlayer1(mousPos);
-        }
-
-#endif
-
         foreach (Touch touch in touches)
         {
-            Debug.Log("ForEach");
-            if (touch.phase == TouchPhase.Ended)
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
             {
-                reinitIDPlayer(touch);
-            }
-            else
-            {
-                if (touch.fingerId == fingerIDPlayer1)
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                Vector3 targetPosition = new Vector3();
+                if (Physics.Raycast(ray, out hit))
                 {
-                    playerManager.movePlayer1(touch.position);
+                    if(playerManager.mPlayer1.gameObject == hit.transform.gameObject)
+                    {
+                        playerManager.movePlayer1(targetPosition, hit);
+                    }
+                    if (playerManager.mPlayer2.gameObject == hit.transform.gameObject)
+                    {
+                        playerManager.movePlayer2(targetPosition, hit);
+                    }
                 }
-                /*else if (touch.fingerId == fingerIDPlayer2)
-                {
-                    playerManager.movePlayer2(touch.position);
-                }*/
             }
         }
-    }
-
-    private void reinitIDPlayer(Touch touch)
-    {
-        if (touch.fingerId == fingerIDPlayer1)
-        {
-            fingerIDPlayer1 = -1;
-        }
-        else if (touch.fingerId == fingerIDPlayer2)
-        {
-            fingerIDPlayer2 = -1;
-        }
-    }
-
-    private bool allPlayersTouching()
-    {
-        return fingerIDPlayer1 != -1 && fingerIDPlayer2 != -1;
-    }
-
-    private void assignNewId()
-    {
-        if(fingerIDPlayer1 == -1)
-        {
-            fingerIDPlayer1 = playerManager.getIDTouchInRangePlayer1(touches);
-        }
-        if(fingerIDPlayer2 == -1)
-        {
-            fingerIDPlayer2 = playerManager.getIDTouchInRangePlayer2(touches);
-        }
-    }
-
-    private void getFirstIDFinger()
-    {
-        fingerIDPlayer1 = playerManager.getIDTouchInRangePlayer1(touches);
-        fingerIDPlayer2 = playerManager.getIDTouchInRangePlayer2(touches);
     }
 }
--- New Unity Project/Assets/Scripts/Manager/PlayerManager.cs
+++ New Unity Project/Assets/Scripts/Manager/PlayerManager.cs
@@ -18,37 +18,18 @@ public class PlayerManager : MonoBehaviour
         colliderPlayer2 = mPlayer2.GetComponent<Collider>();
     }
 
-    public void movePlayer1(Vector3 touchposition)
+    public void movePlayer1(Vector3 targetPosition, RaycastHit hit)
     {
-        Debug.Log("In MovePlayer1" + touchposition);
-        Debug.Log("tatata " + mPlayer1.transform.position);
-        
-        Vector3 direction = new Vector3(touchposition.x - mPlayer1.transform.position.x - Screen.width / 2, 
-                                        0,
-                                        touchposition.y - Screen.height/2 - mPlayer1.transform.position.y);
-
-        Debug.Log("Super Biclette " + direction);
-
-        direction = direction.normalized * mPlayer1.getSpeed();
-        
-
-        mPlayer1.transform.position += direction;
-
-        mPlayer1.transform.LookAt(direction);
-        Debug.Log("Biclette " + direction);
+        targetPosition = hit.point;
+        targetPosition.y = -1f;
+        mPlayer1.transform.position = targetPosition;
     }
     
-    public void movePlayer2(Vector3 touchposition)
+    public void movePlayer2(Vector3 targetPosition, RaycastHit hit)
     {
-        Vector3 direction = new Vector3(touchposition.x - mPlayer2.transform.position.x,
-                                        touchposition.y - mPlayer2.transform.position.y,
-                                        mPlayer2.transform.position.z);
-
-        direction = direction.normalized * mPlayer2.getSpeed();
-
-        mPlayer2.transform.position += direction;
-
-        mPlayer2.transform.LookAt(direction);
+        targetPosition = hit.point;
+        targetPosition.y = -1f;
+        mPlayer2.transform.position = targetPosition;
     }
 
     /*private void buildPlayer1(List<Spell> spellsChoosen)
@@ -60,34 +41,4 @@ public class PlayerManager : MonoBehaviour
     {
         return 0;
     }*/
-
-    public int getIDTouchInRangePlayer1(Touch[] touches)
-    {
-        foreach(Touch touch in touches)
-        {
-            Vector3 pos = touch.position;
-            pos.z = touch.position.y;
-            pos.y = mPlayer1.transform.position.z;
-            if (colliderPlayer1.bounds.Contains(pos))
-            {
-                return touch.fingerId;
-            }
-        }
-        return -1;
-    }
-
-    public int getIDTouchInRangePlayer2(Touch[] touches)
-    {
-        foreach (Touch touch in touches)
-        {
-            Vector3 pos = touch.position;
-            pos.z = touch.position.y;
-            pos.y = mPlayer2.transform.position.z;
-            if (colliderPlayer2.bounds.Contains(pos))
-            {
-                return touch.fingerId;
-            }
-        }
-        return -1;
-    }
 }
